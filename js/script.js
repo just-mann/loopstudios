a733e18928ca9b45a12539e43559aa2bfcc07c83
Justin <64009695+just-mann@users.noreply.github.com>
2021-08-26 17:12:02 -0400

    Added the click function in JavaScript to Open and Close the menu

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-console.log('It works');
+let openBtn = document.getElementById('openMenu');
+let closeBtn = document.getElementById('closeMenuBtn');
+let menu = document.getElementById('menu');
+
+// Function to Open the Responsive Menu
+function openMenu() {
+  menu.style.display = 'block';
+}
+
+// Function to Close the Responsive Menu
+function closeMenu() {
+  menu.style.display = 'none';
+}
+
+// Open the Responsive Menu
+openBtn.addEventListener('click', openMenu);
+
+// Close the Responsive Menu
+closeBtn.addEventListener('click', closeMenu);
